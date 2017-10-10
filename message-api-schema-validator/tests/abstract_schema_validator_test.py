2cf2267a2b30f41d11561bc9644ce7fa53d8934f
Daniel Grant <daniel.grant@digirati.com>
2017-10-10 11:22:34 +0100

    [RDSS-1515] Enhanced the JSON schema to enforce stricter validation.

--- message-api-schema-validator/tests/abstract_schema_validator_test.py
+++ message-api-schema-validator/tests/abstract_schema_validator_test.py
@@ -13,11 +13,13 @@ class AbstractSchemaValidatorTest(TestCase):
     schema_intellectual_asset_id = 'https://www.jisc.ac.uk/rdss/schema/intellectual_asset.json/#';
     schema_material_asset_id = 'https://www.jisc.ac.uk/rdss/schema/material_asset.json/#';
     schema_research_object_id = 'https://www.jisc.ac.uk/rdss/schema/research_object.json/#';
+    types_id = 'https://www.jisc.ac.uk/rdss/schema/types.json/#';
 
     schema_enumeration_path = 'schemas/enumeration.json'
     schema_intellectual_asset_path = 'schemas/intellectual_asset.json'
     schema_material_asset_path = 'schemas/material_asset.json'
     schema_research_object_path = 'schemas/research_object.json'
+    types_path = 'schemas/types.json'
 
     # base_path = argv[1]
     # Replaced above with below to pass tests as argv[1] is not set when this is run, change made to gain pass in travis
@@ -34,13 +36,31 @@ class AbstractSchemaValidatorTest(TestCase):
     def validate_json(self, json_file_name):
         json_schema = self.get_json(self.get_json_schema_file_name())
         json_data = self.get_json(json_file_name)
-        validate(json_data, json_schema, resolver=RefResolver('', {}, store={
-            self.schema_enumeration_id: self.get_json(self.schema_enumeration_path),
-            self.schema_intellectual_asset_id: self.get_json(self.schema_intellectual_asset_path),
-            self.schema_material_asset_id: self.get_json(self.schema_material_asset_path),
-            self.schema_research_object_id: self.get_json(
-                self.schema_research_object_path)
-        }))
+        validate(
+            json_data,
+            json_schema,
+            resolver=RefResolver(
+                '',
+                {},
+                store={
+                    self.schema_enumeration_id: self.get_json(
+                        self.schema_enumeration_path
+                    ),
+                    self.schema_intellectual_asset_id: self.get_json(
+                        self.schema_intellectual_asset_path
+                    ),
+                    self.schema_material_asset_id: self.get_json(
+                        self.schema_material_asset_path
+                    ),
+                    self.schema_research_object_id: self.get_json(
+                        self.schema_research_object_path
+                    ),
+                    self.types_id: self.get_json(
+                        self.types_path
+                    )
+                }
+            )
+        )
 
     def get_json(self, file_name):
         with open(self.prepare_file_name(file_name)) as json_data:
